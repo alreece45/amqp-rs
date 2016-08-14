036cf97aad5c28c626d9689e7a055509563e2acd
Alexander Reece <github@reece45.com>
2016-08-14 06:48:03 -0400

    Licensing

--- xmlgen/src/amqp0/protocol/protocol.rs
+++ xmlgen/src/amqp0/protocol/protocol.rs
@@ -8,6 +8,13 @@ use common::ConstantGroup;
 use super::Constant;
 
 pub struct Protocol<'a>(parsed::Protocol<'a>);
+// Copyright 2016 Alexander Reece
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
 impl<'a> Protocol<'a> {
     pub fn new(protocol: parsed::Protocol<'a>) -> Self {
