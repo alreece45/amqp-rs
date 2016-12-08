ba1c8a85631970699405af2ec48b7247f6aeb60f
Alexander Reece <github@reece45.com>
2016-12-07 20:05:58 -0500

    [primitives] Add better handling for Table and List parsing errors.

--- primitives/src/amqp0/field/nom.rs
+++ primitives/src/amqp0/field/nom.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 use std::mem;
 use std::str;
 
-use nom::IResult;
+use nom::{ErrorKind, IResult};
 use nom::{be_f32, be_f64};
 use nom::{be_u8, be_u16, be_u32, be_u64};
 use nom::{be_i8, be_i16, be_i32, be_i64};
@@ -18,6 +18,12 @@ impl<'a> NomBytes<'a> for Value<'a> {
     fn nom_bytes<'b, P>(input: &'a [u8], pool: &'b mut P) -> IResult<&'a [u8], Self>
         where P: ParserPool
     {
+        let char = try_parse!(input,
+            peek!(take_str!(1))
+        );
+        println!("Value Type: {:?}", char.1);
+        println!("Value Bytes: {:?}", char.0);
+
         let result = switch!(input, take!(1),
             b"s" => map!(shortstr, |s: &'a str| Value::ShortString(Cow::Borrowed(s))) |
             b"S" => map!(longstr, |b: &'a [u8]| Value::LongString(Cow::Borrowed(b))) |
@@ -38,28 +44,71 @@ impl<'a> NomBytes<'a> for Value<'a> {
             b"F" => map!(call!(Table::nom_bytes, pool), Value::Table) |
             b"A" => map!(call!(List::nom_bytes, pool), Value::List)
         );
+        println!("Value: {:?}", result);
         result
     }
 }
 
 type TableEntry<'a> = (&'a str, Value<'a>);
 
+impl<'a> NomBytes<'a> for List<'a> {
+    /// TODO: Return error if there is junk at the end
+    fn nom_bytes<'b, P>(input: &'a [u8], pool: &'b mut P) -> IResult<&'a [u8], Self>
+        where P: ParserPool
+    {
+        let (input, len) = try_parse!(input, be_u32);
+        if len == 0 {
+            return IResult::Done(input, List::from_vec(vec![]));
+        }
+
+        println!("Length: {:?}", len);
+        println!("Input: {:?}", input);
+        let (_, bytes) = try_parse!(input, peek!(length_bytes!(value!(len))));
+        println!("Payload: {:?}", bytes);
+
+        #[ignore(unused_variables)]
+        let (rem, entries): (&'a [u8], Vec<Value<'a>>) = try_parse!(input,
+            length_value!(value!(len),
+                terminated!(
+                    many0!(apply!(Value::nom_bytes, pool)),
+                    eof!()
+                )
+            )
+        );
+
+        IResult::Done(rem, List::from_vec(entries))
+    }
+}
+
 impl<'a> NomBytes<'a> for Table<'a> {
     fn nom_bytes<'b, P>(input: &'a [u8], pool: &'b mut P) -> IResult<&'a [u8], Self>
         where P: ParserPool
     {
-        let (_, bytes) = try_parse!(input, peek!(length_bytes!(be_u32)));
+        let (input, num_bytes) = try_parse!(input, be_u32);
+        match num_bytes {
+            i if i == 0 => return IResult::Done(input, Table::new()),
+            i if i < 3  => return IResult::Error(ErrorKind::Custom(1)),
+            _ => (),
+        }
+
+        println!("Table Expected Length: {:?}", num_bytes);
+        println!("Table Input: {:?}", input);
+        println!("Table Input Length: {:?}", input.len());
+        let (_, bytes) = try_parse!(input, peek!(length_bytes!(value!(num_bytes))));
 
         #[ignore(unused_variables)]
         let (rem, mut entries) = try_parse!(input,
-            length_value!(be_u32,
-                fold_many0!(
-                    tuple!(shortstr, apply!(Value::nom_bytes, pool)),
-                    pool.new_table_entries_vec(bytes),
-                    |mut entries: Vec<TableEntry<'a>>, entry: TableEntry<'a>| {
-                        entries.push(entry);
-                        entries
-                    }
+            length_value!(value!(num_bytes),
+                terminated!(
+                    fold_many0!(
+                        tuple!(shortstr, apply!(Value::nom_bytes, pool)),
+                        pool.new_table_entries_vec(bytes),
+                        |mut entries: Vec<TableEntry<'a>>, entry: TableEntry<'a>| {
+                            entries.push(entry);
+                            entries
+                        }
+                    ),
+                    eof!()
                 )
             )
         );
@@ -77,67 +126,10 @@ impl<'a> NomBytes<'a> for Table<'a> {
     }
 }
 
-impl<'a> NomBytes<'a> for List<'a> {
-    /// TODO: Return error if there is junk at the end
-    fn nom_bytes<'b, P>(input: &'a [u8], pool: &'b mut P) -> IResult<&'a [u8], Self>
-        where P: ParserPool
-    {
-        let (_, bytes) = try_parse!(input, peek!(length_bytes!(be_u32)));
-
-        #[ignore(unused_variables)]
-        let (rem, entries): (&'a [u8], Vec<Value<'a>>) = try_parse!(input,
-            length_value!(be_u32,
-                fold_many0!(
-                    apply!(Value::nom_bytes, pool),
-                    pool.new_values_vec(bytes),
-                    |mut entries: Vec<Value<'a>>, entry: Value<'a>| {
-                        entries.push(entry);
-                        entries
-                    }
-                )
-            )
-        );
-
-        IResult::Done(rem, List::from_vec(entries))
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
     use amqp0::nom::NoParserPool;
-
-    #[test]
-    fn value_short_string() {
-        let bytes = b"s\x0FThis is a test.";
-        let (_, value) = Value::nom_bytes(bytes, &mut NoParserPool).unwrap();
-        assert_eq!(value, Value::ShortString(Cow::Borrowed("This is a test.")));
-    }
-
-    #[test]
-    fn value_short_string_invalueid() {
-        let bytes = b"s\x12Invalueid UTF-8: \xe2\x28\xa1";
-        assert!(Value::nom_bytes(bytes, &mut NoParserPool).is_err());
-    }
-
-    #[test]
-    fn value_short_string_incomplete() {
-        let bytes = b"s\xffThis is expected to be much longer...";
-        assert!(Value::nom_bytes(bytes, &mut NoParserPool).is_incomplete());
-    }
-
-    #[test]
-    fn value_long_string() {
-        let bytes = b"S\x00\x00\x00\x0FThis is a test.";
-        let (_, value) = Value::nom_bytes(bytes, &mut NoParserPool).unwrap();
-        assert_eq!(value, Value::LongString(Cow::Borrowed(&bytes[5..])));
-    }
-
-    #[test]
-    fn value_long_string_incomplete() {
-        let bytes = b"S\x00\x00\x00\xffThis is expected to be much longer...";
-        assert!(Value::nom_bytes(bytes, &mut NoParserPool).is_incomplete());
-    }
 
     macro_rules! parse_value {
         ($bytes:expr) => { Value::nom_bytes(&$bytes[..], &mut NoParserPool) }
@@ -155,11 +147,11 @@ mod test {
                 panic!("Expected IResult::Done when parsing value, got {:?}", result);
             }
 
-            let result = result.unwrap();
-            if result.0.len() > 0 {
-                panic!("Expected no remaining bytes, got {:?}", result.0)
+            match result {
+                IResult::Done(bytes, _) if bytes.len() > 0 => panic!("Expected no remaining bytes, got {:?}", bytes),
+                IResult::Done(_, value) => value,
+                result => panic!("Expected IResult::Done, got {:?}", result),
             }
-            result.1
         }}
     }
 
@@ -208,6 +200,38 @@ mod test {
                 panic!("Expected result to be IResult::Err, got: {:?}", result)
             }
         }
+    }
+
+    #[test]
+    fn value_short_string() {
+        let bytes = b"s\x0FThis is a test.";
+        let value = parse_done_value!(bytes);
+        assert_eq!(value, Value::ShortString(Cow::Borrowed("This is a test.")));
+    }
+
+    #[test]
+    fn value_short_string_invalueid() {
+        let bytes = b"s\x12Invalueid UTF-8: \xe2\x28\xa1";
+        assert_error!(bytes);
+    }
+
+    #[test]
+    fn value_short_string_incomplete() {
+        let bytes = b"s\xffThis is expected to be much longer...";
+        assert!(Value::nom_bytes(bytes, &mut NoParserPool).is_incomplete());
+    }
+
+    #[test]
+    fn value_long_string() {
+        let bytes = b"S\x00\x00\x00\x0FThis is a test.";
+        let (_, value) = Value::nom_bytes(bytes, &mut NoParserPool).unwrap();
+        assert_eq!(value, Value::LongString(Cow::Borrowed(&bytes[5..])));
+    }
+
+    #[test]
+    fn value_long_string_incomplete() {
+        let bytes = b"S\x00\x00\x00\xffThis is expected to be much longer...";
+        assert!(Value::nom_bytes(bytes, &mut NoParserPool).is_incomplete());
     }
 
     #[test]
@@ -372,7 +396,7 @@ mod test {
         assert_error!(b"A\x00\x00\x00\x08VVVVVs\x02a");
 
         let bytes = b"A\x00\x00\x00\x12VVVVVs\x0Bhello world";
-        let (_, value) = Value::nom_bytes(bytes, &mut NoParserPool).unwrap();
+        let value = parse_done_value_with_length_offset!(bytes, 11);
 
         let list = match value {
             Value::List(ref list) if list.len() == 6 => list,
@@ -389,10 +413,8 @@ mod test {
 
     #[test]
     fn value_table_empty() {
-        assert_incomplete!(b"F\x00\x00\x00");
-
         let bytes = b"F\x00\x00\x00\x00";
-        let (_, value) = Value::nom_bytes(bytes, &mut NoParserPool).unwrap();
+        let value = parse_done_value!(bytes);
 
         match value {
             Value::Table(ref table) if table.len() == 0 => {},
@@ -402,22 +424,8 @@ mod test {
 
     #[test]
     fn value_table_with_entry() {
-        assert_incomplete!(b"F\x00\x00\x00\x01");
-        assert_error!     (b"F\x00\x00\x00\x01\x01");
-        assert_incomplete!(b"F\x00\x00\x00\x02\x01");
-        assert_error!     (b"F\x00\x00\x00\x02\x01a");
-        assert_incomplete!(b"F\x00\x00\x00\x03\x01b");
-        assert_error!     (b"F\x00\x00\x00\x03\x01cs");
-        assert_incomplete!(b"F\x00\x00\x00\x04\x01ds");
-        assert_error!     (b"F\x00\x00\x00\x04\x01es\x01");
-        assert_incomplete!(b"F\x00\x00\x00\x05\x01fs\x01");
-        assert_error!     (b"F\x00\x00\x00\x05\x01gs\x02a");
-        assert_incomplete!(b"F\x00\x00\x00\x06\x01hs\x02b");
-
-        let bytes = b"F\x00\x00\x00\x0B\
-            \x03key\
-            s\x05value";
-        let (_, value) = Value::nom_bytes(bytes, &mut NoParserPool).unwrap();
+        let bytes = b"F\x00\x00\x00\x0B\x03keys\x05value";
+        let value = parse_done_value_with_length_offset!(bytes, 9);
 
         let table = match value {
             Value::Table(ref table) if table.len() == 1 => table,
@@ -430,19 +438,43 @@ mod test {
     #[test]
     fn value_table_with_entries() {
 
-        let bytes = b"A\x00\x00\x00\x12VVVVVs\x0Bhello world";
-        let (_, value) = Value::nom_bytes(bytes, &mut NoParserPool).unwrap();
+        let bytes = b"F\x00\x00\x00\x1E\x01aV\x01bV\x01cV\x01dV\x01eV\x01fs\x0Bhello world";
+        let value = parse_done_value_with_length_offset!(bytes, 22);
 
         let list = match value {
-            Value::List(ref list) if list.len() == 6 => list,
-            value => panic!("Expected list with 6 elements, got {:?}", value)
+            Value::Table(ref table) if table.len() == 6 => table,
+            value => panic!("Expected Table with 6 elements, got {:?}", value)
         };
 
-        assert_eq!(list.get(0), Some(&Value::Void));
-        assert_eq!(list.get(1), Some(&Value::Void));
-        assert_eq!(list.get(2), Some(&Value::Void));
-        assert_eq!(list.get(3), Some(&Value::Void));
-        assert_eq!(list.get(4), Some(&Value::Void));
-        assert_eq!(list.get(5), Some(&Value::ShortString(Cow::Borrowed("hello world"))));
+        assert_eq!(list.get("a"), Some(&Value::Void));
+        assert_eq!(list.get("b"), Some(&Value::Void));
+        assert_eq!(list.get("b"), Some(&Value::Void));
+        assert_eq!(list.get("d"), Some(&Value::Void));
+        assert_eq!(list.get("e"), Some(&Value::Void));
+        assert_eq!(list.get("f"), Some(&Value::ShortString(Cow::Borrowed("hello world"))));
+    }
+
+    #[test]
+    fn test_table_has_no_room_for_value() {
+        // the table is a bit more complicated than the list
+        // we have the table size, size of each key, and size of the value
+        // it is an error if any one of them exceeds what we expect for the table size
+
+        // the table format requires at least a u8, u8+, u8* (3 bytes)
+        // the size of 2 is simply too small to be valid, even if we got more bytes
+        let bytes = b"F\x00\x00\x00\x02";
+        assert_error!(bytes);
+
+        // the expected table length is 3 octets.
+        // the key takes up 3 octets (one for size, 2 for value)
+        // there are no bytes remaining (just the key reaches the expected table length)
+        //
+        // ideally, this should fail even without the "aa" at the end
+        // but we don't parse the table at all until we have the entire thing
+        let bytes = b"F\x00\x00\x00\x03\x02aa";
+        assert_error!(bytes);
+
+        let bytes = b"F\x00\x00\x00\x04\x01as\x01";
+        assert_error!(bytes);
     }
 }
