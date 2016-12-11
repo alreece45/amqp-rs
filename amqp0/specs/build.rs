95b53d6f0dc6eceaeb7466341bb25a0c72bf4545
Alexander Reece <github@reece45.com>
2016-12-11 09:31:19 -0500

    [specs] Add license information to build.rs and src/lib.rs

--- amqp0/specs/build.rs
+++ amqp0/specs/build.rs
@@ -1,3 +1,11 @@
+// Copyright 2016 Alexander Reece
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 #![cfg_attr(not(feature="clippy"), allow(unknown_lints))]
