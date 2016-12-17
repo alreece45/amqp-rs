842da1ab97c1795413a547477c22c3cedab4800c
Alexander Reece <github@reece45.com>
2016-12-17 12:51:36 -0500

    [client] Update connect example to test lifetime of start

--- amqp0/client/examples/connect.rs
+++ amqp0/client/examples/connect.rs
@@ -4,10 +4,19 @@ extern crate amqp_client0 as amqp;
 use amqp::ConfigBuilder;
 
 fn main() {
-    let config = ConfigBuilder::without_tls()
+    let mut session = ConfigBuilder::without_tls()
+        .port(5672)
         .plain_auth("", "")
         .channel_max(10)
-        .virtual_host("/");
+        .virtual_host("/")
+        .into_blocking();
 
-    print!("{:#?}", config);
+    print!("{:#?}", session);
+    {
+        let start1 = session.connect().unwrap();
+        print!("Start1: {:?}", start1);
+    }
+
+    let start2 = session.connect().unwrap();
+    print!("Start2: {:?}", start2);
 }
